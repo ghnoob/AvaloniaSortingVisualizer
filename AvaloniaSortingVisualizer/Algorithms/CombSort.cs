1fe9fefe9d64daff1abc932570219f323044c837
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2023-05-30 17:29:10 -0300

    feat(Algorithms): add comb sort

---
+++ AvaloniaSortingVisualizer/Algorithms/CombSort.cs
@@ -0,0 +1,35 @@
+﻿using System.Threading.Tasks;
+using AvaloniaSortingVisualizer.Services;
+
+namespace AvaloniaSortingVisualizer.Algorithms
+{
+    public class CombSort : BubbleSort
+    {
+        private const double ShrinkFactor = 1.3;
+
+        public CombSort(ISoundService soundService)
+            : base(soundService) { }
+
+        public override async Task RunRange(int start, int end)
+        {
+            for (
+                int gap = (int)((end - start) / ShrinkFactor);
+                gap > 1;
+                gap = (int)(gap / ShrinkFactor)
+            )
+            {
+                for (int i = start + gap; i < end; i++)
+                {
+                    if (Compare(Items[i], Items[i - gap]) < 0)
+                        await Swap(i, i - gap);
+                    else
+                        await UpdateBox(i);
+                }
+            }
+
+            await base.RunRange(start, end);
+        }
+
+        public override string ToString() => "Comb Sort";
+    }
+}
--- AvaloniaSortingVisualizer/App.axaml.cs
+++ AvaloniaSortingVisualizer/App.axaml.cs
@@ -49,6 +49,7 @@ public partial class App : Application
 
         // Algorithms
         services.AddTransient<SortingAlgorithm, BubbleSort>();
+        services.AddTransient<SortingAlgorithm, CombSort>();
 
         services.AddTransient<Shuffle, FisherYatesShuffle>();
 

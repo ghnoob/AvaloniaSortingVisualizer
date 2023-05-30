cdeb93917df50d739495127bbf316b3eb4884e89
Rodrigo Pietnechuk <ptnchk.rodrigo@gmail.com>
2023-05-30 17:01:43 -0300

    refactor(Algorithms): remove unnnecesary level of inheritance

--- AvaloniaSortingVisualizer/Algorithms/FisherYatesShuffle.cs
+++ AvaloniaSortingVisualizer/Algorithms/FisherYatesShuffle.cs
@@ -1,21 +1,31 @@
 ﻿using System.Threading.Tasks;
 using AvaloniaSortingVisualizer.Services;
 
+using System;
+
 namespace AvaloniaSortingVisualizer.Algorithms
 {
     /// <summary>
     /// Implementation of the Fisher-Yates shuffle algorithm
     /// </summary>
-    public class FisherYatesShuffle : RandomShuffle
+    public class FisherYatesShuffle : Shuffle
     {
+        /// <summary>
+        /// Gets the random number generator (RNG)
+        /// </summary>
+        private Random rng { get; }
+
         public FisherYatesShuffle(ISoundService soundService)
-            : base(soundService) { }
+            : base(soundService)
+        {
+            rng = new Random();
+        }
 
         public override async Task RunRange(int start, int end)
         {
             for (int i = start; i < end - 1; i++)
             {
-                int j = Rng.Next(i, end);
+                int j = rng.Next(i, end);
                 await Swap(i, j);
             }
         }
